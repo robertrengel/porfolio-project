6dc51e54eac201fa65fcc974fe3bef0be3a749cc
robertrengel <robertrengel@gmail.com>
2024-01-23 08:28:30 -0500

    project-porfolio 0.11

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import "./style.css";
 import "normalize.css"
 
+var contactosId = document.getElementById("contactos-msg")
+var button = document.querySelector(".button")
+
+button.addEventListener("click", ()=>{
+    contactosId.classList.toggle("visible")
+})
 
 // window.addEventListener("load", function() {
 //     ajustarContenedor()
